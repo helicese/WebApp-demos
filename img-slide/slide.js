ff557fa609c49d2de89aedd4a0157b2ebe20594e
Li Hui <helicese@gmail.com>
2016-07-06 21:04:06 +0800

    add img-slide part

---
+++ img-slide/slide.js
@@ -0,0 +1,17 @@
+var imgList = $('.img-list')[0];
+console.log(imgList);
+
+function slide(argument) {
+	// body...	
+	imgList.css() 
+	console.log('hello');
+}
+
+setTimeout(function(){
+	slide();
+}, 3000);
+
+var slideImg = function (argument) {
+	// body...
+
+}
