2b14875b2c9102589f37b50d0e3f613f349e7a15
helicese <helicese@gmail.com>
2016-07-07 02:25:10 +0800

    demo for the img-slide finished

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 a collection of web application demos, recording the learning process of my front-end study
 
 ###change log
+- 2016.7.7 add img-slide
 - 2016.4.14 add ajax-form
 - 2016.4.11 add cocos-clock
 - 2016.4.10 add position-center
--- img-slide/slide.js
+++ img-slide/slide.js
@@ -1,17 +1,34 @@
-var imgList = $('.img-list')[0];
-console.log(imgList);
+(function(){
+	var btnIndex = 2;
+	$('.nav-btn').each(function(index, item){
+		$(item).click(function(item){
+			slide(index);
+		});
+	});
 
-function slide(argument) {
-	// body...	
-	imgList.css() 
-	console.log('hello');
-}
+	function slide(arg) {
+		if(typeof(arg) === 'number'){
+			btnIndex = arg;
+			changeBtn(btnIndex);
+			$('.img-list').animate({'left': -1020+arg*510}, 500, function(){});		
+		}
+		else {
+			if(btnIndex == 0)
+				btnIndex = 2;
+			else
+				btnIndex -= 1;
+			changeBtn(btnIndex);
+			$('.img-list').animate({'left': "-=510"}, 500, function(){
+				if($('.img-list').css('left') === '-1530px')
+					$('.img-list').css('left', "0px");	
+			});
+		}
+	}
+	function changeBtn(index){
+		$($('.nav-btn')[index]).addClass('active').siblings().removeClass('active');
+	}
 
-setTimeout(function(){
-	slide();
-}, 3000);
-
-var slideImg = function (argument) {
-	// body...
-
-}
+	setInterval(function(){
+		slide();
+	}, 3000);
+})();
